d4fb87045f850d853852f69df15b7fa974ed7910
Anders Urban <minikaliffen@gmail.com>
2020-09-07 01:30:50 +0200

    Service layer added

---
+++ WHTracker.Services/ZKillAPIExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WHTracker.Services
+{
+    public static class ZKillAPIExtension
+    {
+        public static IServiceCollection AddZKillAPI(this IServiceCollection services)
+        {
+
+            return services;
+        }
+    }
+}
