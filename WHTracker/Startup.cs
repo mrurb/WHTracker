f8d336e5e79b3e1824188ada9cc9319108063170
Anders Urban <minikaliffen@gmail.com>
2020-09-07 19:42:14 +0200

    Adde cahche

--- WHTracker/Startup.cs
+++ WHTracker/Startup.cs
@@ -39,6 +39,7 @@ namespace WHTracker
                     Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddSingleton<AggregateCache>();
+            services.AddScoped<AggregateCacheManagerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
