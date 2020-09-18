827c28fdf28a7aa6a35b844a344b100007c9a626
Anders Urban <minikaliffen@gmail.com>
2020-09-18 03:07:59 +0200

    First attemp at generic service.

--- WHTracker.Services/Cache/AggregateCache.cs
+++ WHTracker.Services/Cache/AggregateCache.cs
@@ -1,4 +1,5 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,50 @@ using WHTracker.Data.Models;
 
 namespace WHTracker.Services.Cache
 {
-    public class AggregateCache <T>
+    public class AggregateCache <T> where T : AggregateData
     {
+        private readonly ApplicationContext applicationContext;
+        internal DbSet<T> dbset;
 
         public List<(DateTime day, DateTime lastPulled, IEnumerable<T> dailyAggregateCorporation)> dailyAggregateCorporations { get; set; }
 
-        public AggregateCache()
+        public AggregateCache(ApplicationContext applicationContext)
         {
             dailyAggregateCorporations = new List<(DateTime day, DateTime lastPulled, IEnumerable<T> dailyAggregateCorporation)>();
+            this.applicationContext = applicationContext;
+            dbset = applicationContext.Set<T>();
+        }
+
+        public async Task<(DateTime day, DateTime lastPulled, IEnumerable<T> dailyAggregateCorporation)> GetAggregateCorporationout(DateTime dateTime)
+        {
+            (DateTime day, DateTime lastPulled, IEnumerable<T> dailyAggregateCorporation)? aggregate = GetAggregateCorporation(dateTime);
+
+            if (aggregate.Value.dailyAggregateCorporation is not null)
+            {
+                if (aggregate.Value.lastPulled < DateTime.UtcNow.AddMinutes(-5))
+                {
+                    (DateTime Date, DateTime UtcNow, List<T> lists) newAggregate = await GetDACFromDatabase(dateTime);
+                    Update(newAggregate);
+                    return newAggregate;
+                }
+                else
+                {
+                    return aggregate.Value;
+                }
+            }
+            else
+            {
+                (DateTime Date, DateTime UtcNow, List<T> lists) newAggregate = await GetDACFromDatabase(dateTime);
+                Add(newAggregate);
+                return newAggregate;
+            }
+        }
+
+        private async Task<(DateTime Date, DateTime UtcNow, List<T> lists)> GetDACFromDatabase(DateTime dateTime)
+        {
+            List<T> lists = await dbset.Where(c => c.TimeStamp.Date == dateTime.Date).ToListAsync();
+            var newAggregate = (dateTime.Date, DateTime.UtcNow, lists);
+            return newAggregate;
         }
 
         public (DateTime day, DateTime lastPulled, IEnumerable<T> dailyAggregateCorporation)? GetAggregateCorporation(DateTime dateTime)
